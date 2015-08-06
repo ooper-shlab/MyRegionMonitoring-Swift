0219024cc0e5154266dfb5ce0239cc9d93e5671b
OOPer <nagata@shlab.jp>
2015-08-06 21:15:11 +0900

    init

---
+++ MyRegionMonitoring/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  MyRegionMonitoring
+//
+//  Created by OOPer in cooperation with shlab.jp, on 2015/8/5.
+//  Copyright © 2015 OOPer (NAGATA, Atsuyuki). All rights reserved. See LICENSE.txt.
+//
+
+//http://stackoverflow.com/questions/15590655/how-to-use-geo-based-push-notifications-on-ios
+
+import UIKit
+import CoreLocation
+
+private let JS_LAT: CLLocationDegrees = 34.649394
+private let JS_LON: CLLocationDegrees = 135.001478
+
+private let REGION_RADIUS: CLLocationDistance = 200
+private let coord: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: JS_LAT, longitude: JS_LON)
+private let FILTERED_DISTANCE: CLLocationDistance = 10
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet var textField: UITextField!
+    
+    private let locationManger: CLLocationManager = CLLocationManager()
+    private let region: CLCircularRegion = CLCircularRegion(center: coord, radius: REGION_RADIUS, identifier: "JS")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        locationManger.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == CLAuthorizationStatus.NotDetermined {
+            locationManger.requestAlwaysAuthorization()
+        }
+        
+        if #available(iOS 9.0, *) {
+            locationManger.allowsBackgroundLocationUpdates = true
+        }
+        locationManger.startMonitoringForRegion(region)
+        
+        locationManger.desiredAccuracy = kCLLocationAccuracyBest
+        locationManger.distanceFilter = FILTERED_DISTANCE
+        locationManger.startUpdatingLocation()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        NSLog(__FUNCTION__)
+        locationManger.startUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didExitRegion region: CLRegion) {
+        NSLog(__FUNCTION__)
+        locationManger.stopUpdatingLocation()
+        dispatch_async(dispatch_get_main_queue()) {
+            self.textField.text = "Exited"
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        NSLog(__FUNCTION__)
+        if let location = locations.last {
+            if !self.region.containsCoordinate(location.coordinate) {
+                locationManger.stopUpdatingLocation()
+            } else {
+                let message = "Arrived at \(region.identifier) on \(NSDate())"
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.textField.text = message
+                }
+                
+                let localNotification = UILocalNotification()
+                localNotification.alertBody = message
+                localNotification.alertAction = "Arrived at destination!"
+                localNotification.hasAction = true
+                UIApplication.sharedApplication().presentLocalNotificationNow(localNotification)
+            }
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,18 @@
+# MyRegionMonitoring
+A simple example of region monitoring using CLLocationManager
+
+Created by OOPer in cooperation with shlab.jp, on 2015/8/6.
+
+Rewrite the coordinate (JS_LAT, JS_LON) in ViewController.swlft.
+
+(Check Info.plist, before you import some code into your app.)
+
+===========================================================================
+BUILD REQUIREMENTS:
+
+Xcode 7 beta 4, iOS SDK 9
+
+===========================================================================
+RUNTIME REQUIREMENTS:
+
+iOS 8.0 or later
